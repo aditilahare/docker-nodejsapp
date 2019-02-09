33b598b34e644921837ef075a5874756bd39a2c4
Aditi Lahare <aditilahare@Aditi-Lahare.local>
2019-02-09 12:48:36 +0530

    Added Dockerfile to containerize the todo app.

--- todoApp/src/main/java/com/example/todoApp/Controller/TodoController.java
+++ todoApp/src/main/java/com/example/todoApp/Controller/TodoController.java
@@ -1,4 +1,4 @@
-package com.example.Controller;
+package com.example.todoApp.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,14 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TodoController {
 
-    @RequestMapping(value = "/foo", method = RequestMethod.GET)
-    public String sayHello(){
-        return "<p> Hello </p> </p><form action='/todos' method='post'> <input name='name' type='text'>" +
-                "<button> Save </button></form>";
-    }
-
     @RequestMapping(value = "/todos", method = RequestMethod.GET)
     public String getTodos(){
-        return "{Buy : Milk , Visit : Doctor}";
+        return "{Title:Buy Milk, Description:Today evening}";
     }
+
 }
