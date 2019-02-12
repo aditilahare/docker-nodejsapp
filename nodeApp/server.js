cc9048fa8dd645af2f6f9678430ae14af2843282
Aditi Lahare <aditilahare@Aditi-Lahare.local>
2019-02-12 20:42:24 +0530

    Added post request to save newly added todo.

--- nodeApp/server.js
+++ nodeApp/server.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const port = 3000;
 const app = express();
+const request = require('request');
+const bodyParser = require('body-parser');
 
-app.get("/", (req,res) =>{
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(port, ()=>{
+app.get("/todos", (req, res) => {
+    let form = "<form action='/todos' method='POST'>" + "Title: <input name=title type='text'> " + "Description: <input name=description type='text'> " +
+        "<button> Add </button></form>" + "\n" + "<b>Your Todos:</b>";
+
+    request.get('http://todoapp:8080/todos', (error, response, body) => {
+        if (error) return console.log(error);
+        const content = `${form}<br/><br/>${body}`;
+        res.send(content);
+    });
+});
+
+
+app.post('/todos', (req, res) => {
+    request.post('http://todoapp:8080/todos', {json: {Title: req.body.title, Description: req.body.description}},
+        (error, response, body) => {
+            if (error) console.log(error);
+            res.redirect('/todos');
+        });
+});
+
+app.listen(port, () => {
     console.log(`App listening on ${port}`);
 });
