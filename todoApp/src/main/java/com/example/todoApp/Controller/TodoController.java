3df99b2f7d134ca56524e8e140193f07d0e8e343
Aditi Lahare <aditilahare@Aditi-Lahare.local>
2019-02-12 20:43:33 +0530

    Handled post mapping in todo app and store the todos.

--- todoApp/src/main/java/com/example/todoApp/Controller/TodoController.java
+++ todoApp/src/main/java/com/example/todoApp/Controller/TodoController.java
@@ -1,15 +1,38 @@
 package com.example.todoApp.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class TodoController {
 
-    @RequestMapping(value = "/todos", method = RequestMethod.GET)
-    public String getTodos(){
-        return "{Title:Buy Milk, Description:Today evening}";
+    private String getTodos() throws IOException {
+        Stream<String> todos = Files.lines(Paths.get("./todo.json"), StandardCharsets.UTF_8);
+        return todos.collect(Collectors.joining(""));
     }
 
+    @GetMapping(value = "/todos")
+    public Object showTodos() throws IOException {
+        return getTodos();
+    }
+
+    @PostMapping(value = "/todos")
+    public String addTodos(@RequestBody Object currentTodo) throws IOException, JSONException {
+        JSONArray existingTodos = new JSONArray(getTodos());
+        existingTodos.put(currentTodo);
+        Files.write(Paths.get("./todo.json"), Collections.singleton(existingTodos.toString()));
+        return currentTodo.toString();
+    }
 }
